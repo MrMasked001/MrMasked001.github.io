36c00541a1b45e1c9d9e074fc991b7ad5047a8cf
Masked <arnavsaini117@gmail.com>
2021-09-30 14:02:36 +0530

    NEWW

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,27 @@
 $(Document).ready(function() {
 
+    $('#intro').fadeIn(1000).delay(2000).fadeOut(function() {
+
+        $('#load').fadeIn(1000)
+        $('#close').fadeIn(1000)
+
+    });
 
 
 
 });
+
+function finished() {
+
+    $('#load').fadeOut(function() {
+
+        $(".loader").fadeOut();
+        $('#done').fadeIn(1000).delay(2000).fadeOut(function() {
+
+            $('#close').fadeIn(1000)
+
+        });
+
+    });
+
+};
